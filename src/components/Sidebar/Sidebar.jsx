908e705deb9ce76d42bc9e16219c098eaf2ef3ac
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-03 17:22:48 -0300

    Componente Sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <nav id='sidebar'>
+            <ul>
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Sidebar;
