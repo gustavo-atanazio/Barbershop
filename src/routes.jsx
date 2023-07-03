c83a3bdff6e7aca8b9dae85a106be55aa13b860f
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-03 17:24:49 -0300

    Sidebar Provider

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { SidebarProvider } from "./context/sidebarContext";
 import Home from "./pages/Home";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-            </Routes>
+            <SidebarProvider>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </SidebarProvider>
         </BrowserRouter>
     )
 }
