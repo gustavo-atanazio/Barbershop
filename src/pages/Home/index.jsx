60868ac28fc87b79bf2bfa8ddee4468beb6b1b97
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-03 17:26:28 -0300

    Exibindo ou ocultando Sidebar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,17 @@
+import { useContext } from "react";
+import { SidebarContext } from "../../context/sidebarContext";
+
 import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 function Home() {
+  const {isOpen} = useContext(SidebarContext);
+
   return (
-    <Header/>
+    <>
+      <Header/>
+      {isOpen && <Sidebar/>}
+    </>
   )
 }
 
