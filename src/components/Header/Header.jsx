9e6291c09f7aaff564f47c9ecb8365e6adb8d317
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-03 17:25:57 -0300

    Exibindo ou ocultando Sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
+import { useContext } from 'react';
+import { SidebarContext } from '../../context/sidebarContext';
+
 import './Header.css';
 import barberLogo from '../../assets/barber_logo.png';
 
+import { RxHamburgerMenu } from 'react-icons/rx';
+
 function Header() {
-  return (
-    <header>
-        <img src={barberLogo} alt="Logo da Barbershop"/>
-    </header>
-  )
+    const {toggle} = useContext(SidebarContext);
+
+    return (
+        <header id='header'>
+            <button onClick={() => toggle()}>
+                <RxHamburgerMenu/>
+            </button>
+            <img src={barberLogo} alt="Logo da Barbershop"/>
+        </header>
+    )
 }
 
 export default Header;
