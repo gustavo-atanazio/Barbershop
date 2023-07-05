72d251ff455140bec4a74e934ae24c7dd431c713
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-05 15:07:45 -0300

    Componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+import barberLogo from '../../assets/barber_logo.png';
+
+function Footer() {
+    return (
+        <footer id='footer'>
+            <img src={barberLogo} alt="Logo da Barbershop"/>
+            <p>&#169; 2023. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer/Footer";
 
 function AppRoutes() {
     return (
@@ -15,6 +16,7 @@ function AppRoutes() {
                 <Route path="/products" element={<Products/>}/>
                 <Route path="/contact" element={<Contact/>}/>
             </Routes>
+            <Footer/>
         </BrowserRouter>
     )
 }
