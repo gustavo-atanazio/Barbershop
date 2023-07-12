f121c5ef6d6fb4724024ce9acfd524b2494e9af2
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-12 17:33:35 -0300

    Componente Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+import './Card.css';
+
+function Card({ imagem, titulo, preco, descricao }) {
+    return (
+        <div className='card'>
+            <div>
+                <img src={imagem} alt={titulo}/>
+            </div>
+
+            <div className="card-info">
+                <h3>{titulo}</h3>
+                <span>{preco}</span>
+                <p>{descricao}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
