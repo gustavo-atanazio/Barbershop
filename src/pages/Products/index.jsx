87531a037592ab1e296901e716bd450a6b72c0c9
gustavo-atanazio <gus.atanazio@gmail.com>
2023-07-12 17:32:41 -0300

    Página de produtos

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,6 +1,43 @@
+import './Products.css';
+import data from '../../data/data';
+import Card from '../../components/Card/Card';
+
 function Products() {
+  const cortes = data[0].cortes;
+  const produtos = data[1].produtos;
+
   return (
-    <h1>Products</h1>
+    <section className="products">
+      <h2>Conheça nossos cortes</h2>
+
+      <div className='container'>
+        {cortes.map((corte, index) => (
+          <Card 
+            imagem={corte.imagem}
+            titulo={corte.titulo}
+            preco={corte.preco}
+            descricao={corte.descricao}
+            
+            key={index}
+          />
+        ))}
+      </div>
+
+      <h2>Encontre os melhores produtos</h2>
+
+      <div className="container">
+        {produtos.map((produto, index) => (
+          <Card
+            imagem={produto.imagem}
+            titulo={produto.titulo}
+            preco={produto.preco}
+            descricao={produto.descricao}
+
+            key={index}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
